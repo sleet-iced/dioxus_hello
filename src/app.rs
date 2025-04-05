712c035e5a1514ac24517383307ecd7cff7a3e91
The SunShining <thesunshiningmin@gmail.com>
2025-04-05 14:45:35 -0500

    simper strart

---
+++ src/app.rs
@@ -0,0 +1,12 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn App() -> Element {
+    rsx! {
+        div { 
+            class: "hello-container",
+            h1 { "Hello, Dioxus!" }
+            p { "Welcome to your simplified Dioxus application." }
+        }
+    }
+}
