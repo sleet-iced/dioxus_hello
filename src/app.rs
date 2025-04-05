e5bb07e8ef26567b0c08bf319ad6df9b526170de
The SunShining <thesunshiningmin@gmail.com>
2025-04-05 15:21:06 -0500

    cleee

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,14 @@
 use dioxus::prelude::*;
 
+const APP_CSS: Asset = asset!("src/css/app.css");
+
 
 #[component]
 pub fn App() -> Element {
     rsx! {
-        div { 
-            class: "hello-container",
+        document::Link { rel: "stylesheet", href: APP_CSS }
+
             h1 { "Hello, Dioxus!" }
             p { "Welcome to your simplified Dioxus application." }
-        }
     }
 }
