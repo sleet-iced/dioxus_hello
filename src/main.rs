1f7fe7c8c6e32c31e98337ea4440006705a83b02
The SunShining <thesunshiningmin@gmail.com>
2025-04-05 15:05:42 -0500

    csss

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 /// ❄️
-/// Hello
+/// dioxus Hello by Sleet.near
+use dioxus::prelude::*;
 
 mod app;
-use app::App;
+use app::App as MainApp;
 
 
+const MAIN_CSS: Asset = asset!("src/css/main.css");
+const FAVICON: Asset = asset!("src/img/sleet_code_icon_trans.png");
 
-fn main() {
+
+#[cfg(not(feature = "web"))]
+pub fn main() {
     dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        MainApp {}
+    }
 }
